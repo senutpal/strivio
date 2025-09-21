7233a2979cae2173407d6ec43cbe371a19f13427
Utpal <utpalsen902@gmail.com>
2025-09-21 18:48:31 +0530

    feat(auth, db): setup authentication and database

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,44 +1,16 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { GithubIcon } from "lucide-react";
+import { auth } from "@/lib/auth";
+import { LoginForm } from "./_components/LoginForm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle className="text-xl">Welcome Back!</CardTitle>
-        <CardDescription>Login with your GitHub account</CardDescription>
-      </CardHeader>
+export default async function LoginPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
-      <CardContent className="flex flex-col gap-4">
-        <Button className="w-full" variant="outline">
-          <GithubIcon size={20} />
-          Sign in with GitHub
-        </Button>
+  if (session) {
+    return redirect("/");
+  }
 
-        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-          <span className="relative z-10 bg-card px-2 text-muted-foreground">
-            Or continue with
-          </span>
-        </div>
-
-        {/* Email Sign In */}
-        <div className="grid gap-3">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input type="email" id="email" placeholder="you@example.com" />
-          </div>
-          <Button className="w-full">Continue with Email</Button>
-        </div>
-      </CardContent>
-    </Card>
-  );
+  return <LoginForm />;
 }
---
+++ lib/auth.ts
@@ -0,0 +1,16 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./db";
+import { env } from "./env";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  socialProviders: {
+    github: {
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    },
+  },
+});
