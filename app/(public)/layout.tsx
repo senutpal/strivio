2dee77b9037ab06d08801032c4154cc78deec0a8
Utpal Sen <utpalsen902@gmail.com>
2025-11-14 15:17:23 +0530

    feat: add courses UI, improve homepage, update theme, and enhance infra/security

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -5,7 +5,9 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
       <Navbar />
-      <main className="container mx-auto px-4 md:px-6 lg:px-8">{children}</main>
+      <main className="container mx-auto px-4 md:px-6 lg:px-8 mb-32">
+        {children}
+      </main>
     </div>
   );
 }
